a6214012e7f6af34b479acd8938aa2f67332ccba
KevinAndresG <3348@holbertonschool.com>
2021-09-06 23:28:51 -0500

    creating the file

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function checks if a singly linked list has a cycle in it
+ * @list: it is the list
+ * Return: an int
+ */
+int check_cycle(listint_t *list)
+{
+}
