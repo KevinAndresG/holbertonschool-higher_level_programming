5671929bef9aa7186fae0e425f4909ad3e1cfe6a
KevinAndresG <3348@holbertonschool.com>
2021-09-08 20:47:58 -0500

    doing some operations

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    ad = ('{}' ' + ' '{}'' = ''{}'.format(a, b, add(a, b)))
+    su = ('{}' ' - ' '{}'' = ''{}'.format(a, b, sub(a, b)))
+    mu = ('{}' ' * ' '{}'' = ''{}'.format(a, b, mul(a, b)))
+    di = ('{}' ' / ' '{}'' = ''{}'.format(a, b, div(a, b)))
+    print("{}".format(ad))
+    print("{}".format(su))
+    print("{}".format(mu))
+    print("{}".format(di))
