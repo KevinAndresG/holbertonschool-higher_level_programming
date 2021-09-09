a4bb02858426e42c5fbdc4a3726b8bc780b3f55d
KevinAndresG <3348@holbertonschool.com>
2021-09-08 22:55:17 -0500

    the big hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import hidden_4, sys
+    sys.path.append('./')
+    for k in dir(hidden_4):
+        if k[0] != "_":
+            print("{}".format(k))
