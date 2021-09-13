d85edb7c4f7e18a5ba4525b3339a574c96d2f1be
KevinAndresG <3348@holbertonschool.com>
2021-09-13 17:34:46 -0500

    recovering a itme of a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,11 @@
 def element_at(my_list, idx):
     listsize = len(my_list)
     sizze = 0
-    if idx > listsize:
+    if idx >= listsize:
         return None
     if idx < 0:
         return None
     else:
         while sizze < idx:
             sizze += 1
-        return listsize
+        return my_list[sizze]
