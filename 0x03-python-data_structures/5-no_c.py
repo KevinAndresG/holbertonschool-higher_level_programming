55f73b6131a66fbda384ca8af01b2976a889a3ee
KevinAndresG <3348@holbertonschool.com>
2021-09-13 23:39:20 -0500

    removing teh cC

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    run = 0
+    while run < len(my_string):
+        run += 1
+        f = my_string.translate({ ord(c): None for c in "cC" })
+        return f
