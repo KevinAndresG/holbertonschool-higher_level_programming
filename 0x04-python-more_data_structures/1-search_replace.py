fedd975d854fab97f0dccdf99703987640689525
KevinAndresG <3348@holbertonschool.com>
2021-09-15 20:18:02 -0500

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-	if my_list == None:
-		return None
-	k = my_list.copy()
-	if search in my_list:
-		for e, i in enumerate(k):
-			if i == search:
-				k[e] = replace
-	return k
+    if my_list == None:
+        return None
+    k = my_list.copy()
+    if search in my_list:
+        for e, i in enumerate(k):
+            if i == search:
+                k[e] = replace
+    return k
