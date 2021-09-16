ac46f013cc53b37522caa11d971a59ac413ddcaf
KevinAndresG <3348@holbertonschool.com>
2021-09-15 20:12:10 -0500

    replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 def search_replace(my_list, search, replace):
-    l = int(search)
-    l2 = int(replace)
+
     k = my_list.copy()
-    for e, i in enumerate(k):
-        if i == l:
-            k[e] = l2
+    if search in my_list:
+        for e, i in enumerate(k):
+            if i == search:
+            	k[e] = replace
     return k
