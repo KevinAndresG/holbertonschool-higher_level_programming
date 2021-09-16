e6397c7c25f0f8ed37b8a218e6e1fb7b74dd5006
KevinAndresG <3348@holbertonschool.com>
2021-09-15 21:43:02 -0500

    uniq number

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,5 +5,5 @@ def uniq_add(my_list=[]):
     size = len(my_list)
     for i in range(0, size - 1):
         if (my_list[i] != my_list[i+1]):
-            sum = sum + my_list[i]
+            sum = sum + my_list[i+1]
     return sum
