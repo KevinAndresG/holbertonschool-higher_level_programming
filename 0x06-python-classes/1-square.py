92701c6483313941a7c3aa6719e0d7c438900b82
KevinAndresG <3348@holbertonschool.com>
2021-09-28 20:52:40 -0500

    fixing size of a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,9 +4,7 @@
 
 class Square:
     __size = None
-"""size of square"""
 
-
-def __init__(self, size=None):
-    self.__size = size
-    print("_Square__size:{}".format(size))
+    def __init__(self, size):
+        """size of a Square"""
+        self.__size = size
