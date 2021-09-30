16d03b23fff6155269b6b63e006f75bf7be6ff93
KevinAndresG <3348@holbertonschool.com>
2021-09-29 21:13:17 -0500

    find area and per

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''Class to Rectangle'''
+
+
+class Rectangle:
+    '''This class is create a rectangle'''
+
+    def __init__(self, width=0, height=0):
+        '''Inicialization of width and height'''
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        '''getter to retrieve the width'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''setter to get the size'''
+        self.__width = value
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        '''getter to retrieve the height'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''setter to get the size'''
+        self.__height = value
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+    def area(self):
+        """take the square of a nimber"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width and self.__height == 0:
+            return 0
+        else:
+            return ((self.__width + self.__height) * 2)
