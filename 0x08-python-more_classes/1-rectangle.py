4fe1de1fa2c5576e83490dded418d172e541c610
KevinAndresG <3348@holbertonschool.com>
2021-09-29 20:31:49 -0500

    fixing the rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         '''Inicialization of width and height'''
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -20,9 +20,9 @@ class Rectangle:
         '''setter to get the size'''
         self.__width = value
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
 
     @property
     def height(self):
@@ -34,6 +34,6 @@ class Rectangle:
         '''setter to get the size'''
         self.__height = value
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be >= 0")
