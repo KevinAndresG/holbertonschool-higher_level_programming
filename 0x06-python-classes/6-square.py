6930b7f71562f7cf903d28c9bb70e9cfac1abc4f
KevinAndresG <3348@holbertonschool.com>
2021-10-02 23:38:44 -0500

    print under score

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""calculate the square of a number"""
+
+
+class Square:
+    """calculate the square of a number"""
+    @property
+    def size(self):
+        """getter func"""
+        return self.__size
+
+    def my_print(self):
+        '''This method print a squere in #'''
+        self.square = self.__size
+        if self.square == 0:
+            print()
+            return
+        for i in range(self.square):
+            for j in range(0, self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__position[1]):
+                if self.__position[1] > 0:
+                    print("", end="")
+                # else:
+                #     print(" ", end="")
+            for i in range(self.square):
+                print("#", end="")
+            print()
+
+    @size.setter
+    def size(self, value):
+        """setter func"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise TypeError("size must be >= 0")
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, value):
+        self.__position = value
+        if type(value) is not tuple:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(value[0]) and type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] and value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Square class"""
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """take the square of a nimber"""
+        return self.__size ** 2
