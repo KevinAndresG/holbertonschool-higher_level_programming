440a377a49aa021afade5ac07945411e0dab2eac
KevinAndresG <3348@holbertonschool.com>
2021-10-11 23:59:19 -0500

    try

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+g
+g
+g
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    g
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
