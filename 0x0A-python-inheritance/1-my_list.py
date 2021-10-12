3221a1534bb05097c4de79408ab2cc3c1e4e16a7
KevinAndresG <3348@holbertonschool.com>
2021-10-11 22:29:10 -0500

    print the sorted list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+print a sorted
+list from a class
+previously added
+"""
+
+
+class MyList(list):
+    """
+    sort the list
+    """
+    def print_sorted(self):
+        print(sorted(self))
