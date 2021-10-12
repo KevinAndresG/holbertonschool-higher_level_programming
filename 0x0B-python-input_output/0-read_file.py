9dcebe4e51ccbaef7bb86705b76593e6191f1369
KevinAndresG <3348@holbertonschool.com>
2021-10-12 16:01:11 -0500

    raed a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+opne and read
+an text file
+"""
+
+
+def read_file(filename=""):
+    """
+    open a file and read
+    """
+    with open(filename) as f:
+        read_data = f.read()
+        print(read_data)
+    f.closed
