16ce00b82bb4998f74593625199306c1dae6e8e0
KevinAndresG <3348@holbertonschool.com>
2021-10-12 01:37:51 -0500

    check if is int

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+create
+an empty
+class
+"""
+
+
+class BaseGeometry():
+    """
+    empty class
+    """
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+
+        if type(self.value) is not int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value <= 0:
+            raise ValueError(
+                "{} must be positivegreater than 0".format(self.name))
