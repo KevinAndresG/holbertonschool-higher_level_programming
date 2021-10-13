a619cdfbd40c22b4877c64894fd4604b341bee87
KevinAndresG <3348@holbertonschool.com>
2021-10-12 19:51:39 -0500

    write at end

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+open
+and write toat end
+"""
+def append_write(filename="", text=""):
+    """
+    open the file
+    """
+    with open(filename, 'a') as file:
+        return file.write(text)
