ff768867537d965f66982ba19ef6a849e9de919c
KevinAndresG <3348@holbertonschool.com>
2021-10-27 22:02:43 -0500

    conditions to the atributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,6 +14,7 @@ class Rectangle(Base):
     """
     a clas to make a rectangle
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
@@ -31,7 +32,7 @@ class Rectangle(Base):
         '''setter to get the size'''
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -45,7 +46,7 @@ class Rectangle(Base):
         '''setter to get the size'''
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -59,7 +60,7 @@ class Rectangle(Base):
         '''setter to get the size'''
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
 
@@ -73,6 +74,6 @@ class Rectangle(Base):
         '''setter to get the size'''
         if type(value) is not int:
             raise TypeError("y must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
