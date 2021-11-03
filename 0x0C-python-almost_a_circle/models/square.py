44da1ab736366a80b82627aabcc400483022e55b
KevinAndresG <3348@holbertonschool.com>
2021-11-02 22:01:02 -0500

    define getter and setter to size

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -24,3 +24,19 @@ class Square(Rectangle):
         """ print the sintaxis """
         return(f"[Square] \
 ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    @property
+    def size(self):
+        """to retrieve it"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """to set it"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
