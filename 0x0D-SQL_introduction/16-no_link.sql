56fa3822af920126f9b2e2e3b0c776e6c2b758ad
KevinAndresG <3348@holbertonschool.com>
2021-11-17 01:38:29 -0500

    dude

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,8 +1,8 @@
 -- list scores and names
 -- in MySQL server
-INSERT INTO second_table (`id`,`name`,`score`) VALUES (5, "Aria", 18);
-INSERT INTO second_table (`id`,`name`,`score`) VALUES (5, "Aria", 12);
-DELETE FROM second_table WHERE score < 10;
 SELECT score, name
-FROM second_table
+FROM
+  second_table
+WHERE
+name IS NOT NULL
 ORDER BY score DESC;
