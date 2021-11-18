a8066d689151769576b7fbc0b8c29241c623131e
KevinAndresG <3348@holbertonschool.com>
2021-11-17 23:28:18 -0500

    create database and table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- create database and table id auto and another things
+-- in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states
+(
+	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+);
