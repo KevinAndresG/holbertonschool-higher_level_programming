01be35d93d9636090522eae046aba0b250a40987
KevinAndresG <3348@holbertonschool.com>
2021-11-18 00:33:51 -0500

    create table and foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- create database and table id auto and another things
+-- in MySQL server
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id),
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
