5f7a49332cb259a77a118b4efefe3a1789aa8fbc
KevinAndresG <3348@holbertonschool.com>
2021-11-18 17:43:20 -0500

    list NULL and NOT NULL shows

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- list all the shows just NULLS
+-- in MySQL server
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
