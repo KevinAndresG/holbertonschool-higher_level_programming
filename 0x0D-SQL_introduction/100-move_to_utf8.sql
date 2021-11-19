421402b19323c644cddb014e146db01131cb4552
KevinAndresG <3348@holbertonschool.com>
2021-11-19 17:38:37 -0500

    convert database table and name field

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+-- convert database table and name field
+-- in MySQL server
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
