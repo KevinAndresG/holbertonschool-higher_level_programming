114593ef16d5cf57f1431353e02536ef6b0cdbcc
KevinAndresG <3348@holbertonschool.com>
2021-11-19 19:49:59 -0500

    list the three tops cities temperatures

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- list the three tops cities temperatures
+-- in MySQL server
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE month BETWEEN 7 AND 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
