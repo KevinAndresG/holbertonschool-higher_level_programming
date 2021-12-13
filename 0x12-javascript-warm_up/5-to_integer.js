2a431ecdf740ddc830fd779577f24b4abacf8cda
KevinAndresG <3348@holbertonschool.com>
2021-12-13 16:43:28 -0500

    casting a string to a number

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
 const args = require('process');
+if (!parseInt(args.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(args.argv[2]));
+}
