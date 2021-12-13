79876d6820eba0c3967f6814edecd106e9dd41cc
KevinAndresG <3348@holbertonschool.com>
2021-12-13 17:28:28 -0500

    multi c is fun inf js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const args = require('process');
+let x = 0;
+
+if (!parseInt(args.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (x; x < parseInt(args.argv[2]); x++) {
+    console.log('C is fun');
+  }
+}
