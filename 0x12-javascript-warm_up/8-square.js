022bca622726850b38840c0636de5850fd29c9ba
KevinAndresG <3348@holbertonschool.com>
2021-12-13 18:20:34 -0500

    print a square with console

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const args = require('process');
+let x = 0;
+let y = 0;
+if (!parseInt(args.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (x = 0; x < parseInt(args.argv[2]); x++) {
+    let square = ''
+    for (y = 0; y < parseInt(args.argv[2]); y++) {
+      square+='X'
+    }
+    console.log(square+" ");
+  }
+}
