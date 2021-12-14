491d02c34cf8baf59614663edfb92d956edd7d15
KevinAndresG <3348@holbertonschool.com>
2021-12-13 23:58:07 -0500

    the factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const args = require('process');
+
+function fact (x) {
+  if (!x) {
+    return 1;
+  }
+  if (x !== 0) {
+    return x * fact(x - 1);
+  } else {
+    return 1;
+  }
+}
+
+const result = fact(parseInt(args.argv[2]));
+if (!args.argv[2]) {
+  console.log(result);
+}
+if (parseInt(args.argv[2]) >= 0) {
+  const result = fact(parseInt(args.argv[2]));
+  console.log(result);
+}
+fact(parseInt(args.argv[2]));
