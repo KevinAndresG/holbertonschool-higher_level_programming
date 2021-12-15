7631522afceaa9bed056b906ef6ae6cf91a306f5
KevinAndresG <3348@holbertonschool.com>
2021-12-15 15:33:59 -0500

    number of ocurrences to an element

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let iter = 0;
+  let count = 0;
+
+  for (iter; iter < list.length; iter++) {
+    if (searchElement === list[iter]) {
+      count++;
+    }
+  }
+  return count;
+};
