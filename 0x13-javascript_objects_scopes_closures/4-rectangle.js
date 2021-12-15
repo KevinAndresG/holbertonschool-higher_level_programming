9504531c50f50f366364535d692266bd7c06c697
KevinAndresG <3348@holbertonschool.com>
2021-12-14 21:32:56 -0500

    new instance methods double and rotate

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,50 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h <= 0) {
+      return 0;
+    } else if (!(w && h)) {
+      return 0;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let x;
+    let y;
+    for (x = 0; x < this.height; x++) {
+      let rect = '';
+      for (y = 0; y < this.width; y++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+
+  rotate () {
+    let x;
+    let y;
+    for (y = 0; y < this.width; y++) {
+      let rect = '';
+      for (x = 0; x < this.height; x++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+
+  double () {
+    let x;
+    let y;
+    this.height *= 2;
+    this.width *= 2;
+    for (x = 0; x < (this.height); x++) {
+      let rect = '';
+      for (y = 0; y < (this.width); y++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+};
