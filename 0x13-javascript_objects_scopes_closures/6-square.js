6541e99318f4266d2ae280629035ba0ee3d9aa81
KevinAndresG <3348@holbertonschool.com>
2021-12-14 23:57:56 -0500

    square if exist if not x

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      super.print(c);
+    } else {
+      super.print('X');
+    }
+  }
+};
