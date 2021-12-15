d8a751291717cae5eac1e79128d97c03fb2817b9
KevinAndresG <3348@holbertonschool.com>
2021-12-15 16:05:48 -0500

    reverse a string

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  let l = list.length;
+  let revlist = [];
+  for (l; l > 0; l--) {
+    revlist += list[l - 1];
+    if (l > 1) revlist += ',';
+  }
+  return revlist.split(',');
+};
