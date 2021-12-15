73ce1c7ef8c6220ffe99d1ee09289eba1f3e34de
KevinAndresG <3348@holbertonschool.com>
2021-12-14 20:57:58 -0500

    printing rectangles

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h <= 0) {
+      return 0;
+    } else if (!(w && h)) {
+      return 0;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let x;
+    let y;
+    for (x = 0; x < this.height; x++) {
+      let rect = '';
+      for (y = 0; y < this.width; y++) {
+        rect += 'X';
+      }
+      console.log(rect);
+    }
+  }
+};
