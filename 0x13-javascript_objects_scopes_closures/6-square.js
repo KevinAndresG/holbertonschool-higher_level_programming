54dc1318f90dd647f48a21ba6e81ac63494c3df3
KevinAndresG <3348@holbertonschool.com>
2021-12-15 11:32:50 -0500

    fixing the code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,9 +7,25 @@ module.exports = class Square extends Rectangle {
 
   charPrint (c) {
     if (c) {
-      super.print(c);
+        let x;
+        let y;
+        for (x = 0; x < this.height; x++) {
+          let rect = '';
+          for (y = 0; y < this.width; y++) {
+            rect += c;
+          }
+          console.log(rect);
+        }
     } else {
-      super.print('X');
+      let x;
+        let y;
+        for (x = 0; x < this.height; x++) {
+          let rect = '';
+          for (y = 0; y < this.width; y++) {
+            rect += 'X';
+          }
+          console.log(rect);
+        }
     }
   }
 };
