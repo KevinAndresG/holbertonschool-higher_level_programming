2ce5be62ef6b21025bc7538d25c4f9d9bbb37e7e
KevinAndresG <3348@holbertonschool.com>
2021-12-14 20:37:15 -0500

    conditioning the values just npositive integers

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h <= 0) {
+      return 0;
+    } else if (!(w && h)) {
+      return 0;
+    }
+    this.width = w;
+    this.height = h;
+  }
+};
