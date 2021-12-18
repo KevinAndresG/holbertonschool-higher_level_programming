61974d1127fd3d531048db69a5c3b8319c2f977f
KevinAndresG <3348@holbertonschool.com>
2021-12-18 17:12:43 -0500

    fixing the code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
 """ 
     list all the data
     from a SQL database
 """
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-    db = MySQLdb.connect(user=sys.argv[1],
+    db = MySQLdb.connect(host = 'localhost', user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
