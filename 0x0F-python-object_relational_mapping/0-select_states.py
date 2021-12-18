a89f703520f37e700f3b827bb4a126b4ea9f09b9
KevinAndresG <3348@holbertonschool.com>
2021-12-18 16:53:51 -0500

    all the empty files and the 0 maded list the database from python script

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM states")
+for n in cursor:
+    all = str(n)
+    print("%s " % all)
+db.close()
