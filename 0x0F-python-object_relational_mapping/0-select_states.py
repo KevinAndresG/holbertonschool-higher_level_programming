7aa9bacc8f128053d6b5466bbfdbd2841a35b0f9
KevinAndresG <3348@holbertonschool.com>
2021-12-18 16:59:30 -0500

    fixing requirements

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                      passwd=sys.argv[2], db=sys.argv[3])
 cursor = db.cursor()
 
-cursor.execute("SELECT * FROM states")
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
 for n in cursor:
     all = str(n)
     print("%s " % all)
