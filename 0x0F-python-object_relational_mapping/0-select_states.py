704951c3fce8dd9a057ad420ce041e91551046aa
KevinAndresG <3348@holbertonschool.com>
2021-12-18 17:07:05 -0500

    add documentacion and not execute when import

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,11 +6,11 @@
 if __name__ == "__main__":
     import MySQLdb
     import sys
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
     for n in cursor:
         all = str(n)
         print("%s " % all)
