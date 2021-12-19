d52c5c74ab611f99542da219bcd8877f9cc048d1
KevinAndresG <3348@holbertonschool.com>
2021-12-18 21:59:46 -0500

    trying to fix 2

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+	list all states
+	that start with N
+"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
+
+    curs = db.cursor()
+
+    curs.execute("SELECT * FROM states WHERE REGEXP_LIKE(UPPER(name), '^[N]') ORDER BY id ASC")
+    justN = curs.fetchall()
+    for n in justN:
+        print(n)
+
+    db.close()
