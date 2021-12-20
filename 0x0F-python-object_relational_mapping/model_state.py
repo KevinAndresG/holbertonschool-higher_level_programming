44b231c24b3258517df3f800784bd1271cd8f9d6
KevinAndresG <3348@holbertonschool.com>
2021-12-20 17:30:38 -0500

    fix

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,14 +5,8 @@
 """
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-import sys
 
 Base = declarative_base()
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
 
 
 class State (Base):
@@ -21,7 +15,5 @@ class State (Base):
         Table named states
     """
     __tablename__ = "states"
-    id = Column(Integer(), nullable=False, primary_key=True)
+    id = Column(Integer(), nullable=False, primary_key=True, unique=True)
     name = Column(String(128), nullable=False)
-    Session = sessionmaker(bind=engine)
-    session = Session()
