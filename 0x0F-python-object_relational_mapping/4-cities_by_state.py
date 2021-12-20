e29afab54770bd73c3c45be090b9e44c6e8d1b32
KevinAndresG <3348@holbertonschool.com>
2021-12-20 12:32:44 -0500

    display all the cities from the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+    list all cities
+    from database
+"""
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], database=sys.argv[3])
+
+    curs = db.cursor()
+    curs.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                INNER JOIN states ON cities.state_id = states.id
+                ORDER BY cities.id ASC""")
+
+    justN = curs.fetchall()
+    for n in justN:
+        print(n)
+
+    db.close()
