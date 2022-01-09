fd495e672656b956d1a5f696ec5511d9cb8fc02a
KevinAndresG <3348@holbertonschool.com>
2022-01-09 18:03:33 -0500

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a GET request to the URL
-curl -s -I GET $1 | grep -i HTTP/ | awk '{if ($2 == 200) print "Route 2"}'
+curl -s -I GET $1 | grep -i HTTP/ | awk '{if ($2 == 200) curl -s $1}'
