1fcc6295ab1163aee84484f4d958c24ca6123e68
KevinAndresG <3348@holbertonschool.com>
2022-01-09 19:12:20 -0500

    sending some variables

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a GET request to the URL
-curl -s $1 | grep -i HTTP/ | awk '{if ($2 == 200) print "Route 2"}'
+grep -i HTTP/ | awk '{if ($2 == 200) print "Route 2"}' | curl -s $1
