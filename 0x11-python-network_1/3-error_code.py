1856a8b1a09a76f31a56785a315317cb3bb65546
KevinAndresG <3348@holbertonschool.com>
2022-01-10 23:43:21 -0500

    handlening errors

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""  """
-import urllib.request
-from sys import argv
+""" Python script that takes in a URL """
+if __name__ == "__main__":
+    import urllib.request
+    from sys import argv
 
-req = urllib.request.Request(argv[1])
-try:
-    with urllib.request.urlopen(req) as response:
-        print(response.read().decode('utf8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
+    req = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
