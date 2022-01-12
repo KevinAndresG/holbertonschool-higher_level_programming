09505921436951a7aebd609232eb9eef30a0bfe5
KevinAndresG <3348@holbertonschool.com>
2022-01-11 19:28:15 -0500

    find the pick

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -6,6 +6,7 @@ list of unsorted integers.
 
 
 def find_peak(list_of_integers):
+    """ reutn the peak of an list """
     if len(list_of_integers) == 0:
         return "None"
     list_of_integers.sort()
