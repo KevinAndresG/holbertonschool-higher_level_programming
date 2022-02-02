f58d942b2aa63922f0a0d66f681707fa22528f18
KevinAndresG <3348@holbertonschool.com>
2022-02-01 22:58:03 -0500

    fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,8 +4,8 @@ const { argv } = require('process');
 const fs = require('fs');
 
 fs.readFile(argv[2], 'utf8', function (err, data) {
-  if (data) {
-    return console.log(data);
+  if (err) {
+    return console.log(err);
   }
-  return console.log(err);
+  console.log(data);
 });
