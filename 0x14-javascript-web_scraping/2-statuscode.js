8bfb6c1eaabc58dd57252c9001da511c8793e0ab
KevinAndresG <3348@holbertonschool.com>
2022-02-02 00:12:22 -0500

    fixing

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,8 @@ const { argv } = require('process');
 const request = require('request');
 const two = argv[2];
 request(two, function (err, res, body) {
-  console.log("code: " + res.statusCode);
+  if (err) {
+    console.log(err);
+  }
+  console.log('code: ' + res.statusCode);
 });
