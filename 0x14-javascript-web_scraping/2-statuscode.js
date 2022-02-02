ee73ebb6facdd2c43394064208923dc9cb56befe
KevinAndresG <3348@holbertonschool.com>
2022-02-01 23:44:37 -0500

    fixing

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const { argv } = require('process');
 const https = require('https');
 
-const response = https.request(argv[2], res => {
-  console.log(`Code: ${res.statusCode}`);
+const response = https.get(argv[2], res => {
+  console.log("Code: " + res.statusCode);
 });
 response.end();
