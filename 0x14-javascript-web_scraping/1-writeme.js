87d3344042e3bdbf47f991c133de6eced8a4e901
KevinAndresG <3348@holbertonschool.com>
2022-02-01 23:13:08 -0500

    fixing

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,7 @@ const { argv } = require('process');
 const fs = require('fs');
 
 fs.writeFile(argv[2], argv[3], 'utf8', function (err) {
-  if (!err) {
-    return console.log(argv[3] > argv[2]);
+  if (err) {
+    return console.log(err);
   }
-  console.log(err);
 });
